47458512559c2b79107dd53c75427e316c025f57
Kobe Mertens <kobe.mertens@hotmail.com>
2025-04-22 11:22:03 +0200

    add utils folder

---
+++ ckanext/transportdata/utils/forms.py
@@ -0,0 +1,6 @@
+def map_for_form_select(tuple_list):
+    """
+    :param tuple_list: array of tuples (key, value)
+    :return: json array suitable for CKAN's form.select
+    """
+    return [{'value': t[0], 'label': t[1]} for t in tuple_list]
