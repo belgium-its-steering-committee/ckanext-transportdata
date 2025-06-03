d14b3164d3727f40d48c3fc02e2891bc418e2462
Kobe Mertens <kobe.mertens@hotmail.com>
2025-06-03 10:16:31 +0200

    allow org members to see other members

--- ckanext/transportdata/helpers/__init__.py
+++ ckanext/transportdata/helpers/__init__.py
@@ -1,3 +1,5 @@
+import ckan.plugins.toolkit as toolkit
+from ckan.logic import NotAuthorized
 
 # Copied from https://github.com/belgium-its-steering-committee/ckanext-scheming/blob/MobilityDCAT/root/ckanext/scheming/helpers.py#L348
 def scheming_split_help_text_by_link(help_text):
@@ -16,3 +18,19 @@ def scheming_split_help_text_by_link(help_text):
     else:
         parts_list.append({"text": help_text, "link": False})
     return parts_list
+
+
+def transportdata_is_member_of_org(org_id, user_id):
+    if not user_id:
+        return False
+    try:
+        members = toolkit.get_action('member_list')({}, {
+            'id': org_id
+        })
+
+        return any(
+            member_type == 'user' and member_id == user_id 
+            for member_id, member_type, _ in members
+        )
+    except NotAuthorized:
+        return False
--- ckanext/transportdata/logic/auth.py
+++ ckanext/transportdata/logic/auth.py
@@ -1,4 +1,5 @@
 import ckan.plugins.toolkit as tk
+import ckan.authz as authz
 
 from logging import getLogger
 log = getLogger(__name__)
@@ -9,4 +10,23 @@ def transportdata_get_sum(context, data_dict):
 
 
 def member_list(context, data_dict):
-    return {"success": False}
+    user = context.get('user')
+    model = context['model']
+    session = model.Session
+
+    org_id = data_dict.get('id')
+
+    if not org_id:
+        return {'success': False, 'msg': 'Organization ID not provided'}
+
+    user_obj = tk.get_action('user_show')({'ignore_auth': True}, {'id': user})
+    if not user_obj:
+        return {'success': False, 'msg': 'User not found'}
+
+    # Check if the user is a member of the org
+    if authz.has_user_permission_for_group_or_org(
+        org_id, user, 'read'
+    ):
+        return {'success': True}
+    
+    return {'success': False, 'msg': 'User is not a member of the organization'}
