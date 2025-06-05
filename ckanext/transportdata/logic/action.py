e61d517e338a0f805c7a0578d774e00e22685b0b
Kobe Mertens <kobe.mertens@hotmail.com>
2025-06-05 16:09:48 +0200

    fix organization_show restriction

--- ckanext/transportdata/logic/action.py
+++ ckanext/transportdata/logic/action.py
@@ -1,6 +1,6 @@
 import ckan.plugins.toolkit as tk
 import ckanext.transportdata.logic.schema as schema
-from ckan.logic.action.get import organization_show
+from ckan.logic.action.get import organization_show as vanilla_organization_show
 from ckan.lib.helpers import url_for_static
 
 
@@ -23,30 +23,28 @@ def transportdata_get_sum(context, data_dict):
 
 # Copied from https://github.com/belgium-its-steering-committee/ckanext-scheming/blob/MobilityDCAT/root/ckanext/scheming/logic.py#L85
 @tk.side_effect_free
-def scheming_organization_show(context, data_dict):
+def organization_show(context, data_dict):
     """
     adjust contents of image_display_url
     """
 
-    result_dict = organization_show(context, data_dict)
-    
-    
-    #API restriction for not logged-in users
-    user_name = context['user']
-    is_user_member_of_org = 0
+    result_dict = vanilla_organization_show(context, data_dict)
 
-    if user_name != '':
-        if user_name == 'napcontrolbody':
-            is_user_member_of_org = 1
-        else:
-            organization_id = result_dict.get('id', None)
-            user_org_dict = tk.get_action('organization_list_for_user')(
-                data_dict={'id': user_name})
-            is_user_member_of_org= len([ id for orgs in user_org_dict if orgs['id'] == organization_id])
-        
-    if user_name == '' or is_user_member_of_org < 1:
-        #data_dict['include_extras'] = False
-        #result_dict = organization_show(context, data_dict)
+    #API restriction for not logged-in users
+
+    user_name = context['user']
+
+    def user_is_member_of_org():
+        if not user_name:
+            return False
+
+        organization_id = result_dict.get('id', None)
+        user_org_dict = tk.get_action('organization_list_for_user')(
+            data_dict={'id': user_name})
+
+        return any(org['id'] == organization_id for org in user_org_dict) 
+
+    if not user_name or (not user_is_member_of_org() and user_name != 'napcontrolbody'):
         #extra pops on NAP request
         popList = ['rtti_doc_document_upload',
                    'srti_doc_document_upload',
